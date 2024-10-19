5a32eb8fe649caa8439409481a7a3d7e98f1a6ef
minhduy20103000 <minhduy20103000@gmail.com>
2024-10-19 09:11:25 +0700

    Delete C2_BT directory

--- C2_BT/C2_BT2.py
+++
@@ -1,6 +0,0 @@
-n = int(input("n = "))
-
-if n % 2 == 0:
-    print(f"{n} (Even)")
-else:
-    print(f"{n} (Odd)")
--- C2_BT/C2_BT3.py
+++
@@ -1,7 +0,0 @@
-a = float(input("a = "))
-b = float(input("b = "))
-
-if a > b:
-    print(f"a > b = {a:.0f}")
-else:
-    print(f"a < b = {b:.0f}")
