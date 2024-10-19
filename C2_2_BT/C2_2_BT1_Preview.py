bd7f40a36d0291226a7e014ccbc4f0e1dc1139a9
minhduy20103000 <minhduy20103000@gmail.com>
2024-10-19 09:17:14 +0700

    Update C2_2_BT1_Preview.py

--- C2_2_BT/C2_2_BT1_Preview.py
+++ C2_2_BT/C2_2_BT1_Preview.py
@@ -1,8 +1,8 @@
 month = int(input("Month: "))
 
-if month == in(1; 3; 5; 7; 8; 10; 12):
+if month == in(1, 3, 5, 7, 8, 10, 12):
     print(f"Month {month} is 31 days")
-elif month == in(4; 6; 9; 11):
+elif month == in(4, 6, 9, 11):
     print(f"Month {month} is 30 days")
 elif month == 2:
     print("Month 2 is 28 days (29 days in Leap Year)")
