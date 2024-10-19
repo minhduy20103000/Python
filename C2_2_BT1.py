c87ebe4315b2b1b4ebf3bee79618ad8d08b0ef42
minhduy20103000 <minhduy20103000@gmail.com>
2024-10-19 08:23:15 +0700

    Add files via upload

---
+++ C2_2_BT1.py
@@ -0,0 +1,28 @@
+month = int(input("Month: "))
+
+if month == 1:
+    print("January is 31 days")
+elif month == 3:
+    print("March is 31 days")
+elif month == 4:
+    print("April is 30 days")
+elif month == 5:
+    print("May is 31 days")
+elif month == 6:
+    print("June is 30 days")
+elif month == 7:
+    print("July is 31 days")
+elif month == 8:
+    print("August is 31 days")
+elif month == 9:
+    print("September is 30 days")
+elif month == 10:
+    print("October is 31 days")
+elif month == 11:
+    print("November is 30 days")
+elif month == 12:
+    print("December is 31 days")
+elif month == 2:
+    print("February is 28 days (29 days in Leap Year)")
+else:
+    print("Error")
